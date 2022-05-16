582fcb12ad428a179cb9dfb447861d798afb32d4
shimonzhion <brys94204@gmail.com>
2022-05-16 09:15:34 +0300

    16.05.22

---
+++ main.js
@@ -0,0 +1,55 @@
+// var numbers = [5, 3, 7, 8, 11];
+// var sum = 0;
+// for (var i = 0; i < 5; i++) {
+//   sum += numbers[i];
+// }
+// document.write(sum / i + "<br>");
+// document.write(sum + "<br>");
+
+// var names = ["nati", "shimon", "lior", "daniel", "dalia"];
+// for (var i = 0; i < 5; i++) {
+//   sum = names;
+// }
+
+// var numArray = [5, 4, 3, 2, 1];
+// function numbersArray(x) {
+//   for (var i = 0; i < numArray.length; i++) {
+//     if (numArray[i] == x) {
+//       console.log(true);
+//     }
+//     else{
+//       console.log(false)
+//     }
+//   }
+// }
+// numbersArray(7);
+//  var numArray = [8,11,23,88,90]
+
+//   function numArray1 (){
+//    for (var i = 0; i<numArray.length;i++){
+//   if (numArray[i]%2==0){
+//     console.log(numArray[i])
+//   }
+//  }}
+//  numArray1()
+//  var num1 =[1,2,3]
+//  var num2 = [4,5,6]
+//  function numArray(){
+//  if (num1.length == num2.length){
+//    num1.push(...num2)
+//    console.log(num1,true)
+//  }
+//  else{
+//    console.log(false)
+//  }
+// }
+// numArray()
+// var numbers = [10, 60, 40, 60, 90,100];
+// function MaxNumArray (){
+// console.log(Math.max(...numbers));
+// }
+// MaxNumArray()
+
+var numbers = [0,1,2,3,4,5,6]
+
+document.write(numbers.slice(1,2))
